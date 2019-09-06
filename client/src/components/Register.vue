30f9d2750d05f4c0acf16581bf97011c726f1219
Nasrul Fuad <nasrulfuad08@gmail.com>
2019-09-06 09:08:07 +0700

    oke

---
+++ client/src/components/Register.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>Register</h1>
+    <input v-model="email" type="text" name="email" placeholder="email" />
+    <br />
+    <input v-model="password" type="password" name="password" placeholder="password" />
+    <br />
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from '@/services/AuthenticationService'
+export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  watch: {
+    email (val) {
+      console.log(val)
+    }
+  },
+  methods: {
+    async register () {
+      await AuthenticationService.register({
+        email: this.email,
+        password: this.password
+      })
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      this.email = 'Hello'
+    }, 1000)
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
--- server/src/app.js
+++ server/src/app.js
@@ -8,8 +8,10 @@ app.use(morgan('combined'))
 app.use(express.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
-  res.json({ message: 'Ahsiyapp' })
+app.post('/register', (req, res) => {
+  res.json({
+    message: `Hello ${req.body.email}! Your user was registered! Have fun!`
+  })
 })
 
 app.listen(process.env.PORT || 8081)
