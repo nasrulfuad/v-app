fa4184f652911e393a4a6143d75953826113869a
Nasrul Fuad <nasrulfuad08@gmail.com>
2019-09-07 17:31:10 +0700

    adding api backend

--- client/src/components/Surahs.vue
+++ client/src/components/Surahs.vue
@@ -2,13 +2,24 @@
   <v-layout column>
     <v-flex xs6 offset-xs3>
       <panel title="Surahs">
+        <div v-for="surah in surahs" :key="surah.name">
+          {{ surah.name }}
+          {{ surah.verse }}
+        </div>
       </panel>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+import SurahsService from '@/services/SurahsService'
 export default {
+  data: () => ({
+    surahs: null
+  }),
+  mounted: async () => {
+    this.surahs = await SurahsService.index()
+  },
   components: {
     Panel: () => import('@/components/Panel')
   }
