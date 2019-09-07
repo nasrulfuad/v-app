d5e18155ddb84c490fa27fbdaaf43e951624df41
Nasrul Fuad <nasrulfuad08@gmail.com>
2019-09-07 16:39:41 +0700

    logout oke

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -18,18 +18,21 @@
         dark
         @click="navigateTo({ name: 'login' })"
         v-if="!$store.state.isUserLoggedIn"
-      >
-          Login
-      </v-btn>
+      >Login</v-btn>
 
       <v-btn
         flat
         dark
         @click="navigateTo({ name: 'register' })"
         v-if="!$store.state.isUserLoggedIn"
-      >
-          Sign Up
-      </v-btn>
+      >Sign Up</v-btn>
+
+      <v-btn
+        flat
+        dark
+        @click="logout"
+        v-if="$store.state.isUserLoggedIn"
+      >Log Out</v-btn>
     </v-toolbar-items>
   </v-toolbar>
 </template>
@@ -39,6 +42,13 @@ export default {
   methods: {
     navigateTo (route) {
       this.$router.push(route)
+    },
+    logout () {
+      this.$store.dispatch('setToken', null)
+      this.$store.dispatch('setUser', null)
+      this.$router.push({
+        name: 'root'
+      })
     }
   }
 }
